2e36f277527f5b9399b561504ef2e234cbe9ac30
amazonikk <o.kozhan.work@gmail.com>
2026-06-03 13:30:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyYv_Vkw3rcYcX59OT7c_gQRUerethLblghxq6_sYVg880BI1zYEAEY4iK68O_jWPUPyA/exec',
   API_TOKEN: 'CHANGE_ME_SECRET_TOKEN',
   REFRESH_INTERVAL_MS: 0, // 0 = автооновлення вимкнене. Наприклад 60000 = раз на хвилину.
 };
